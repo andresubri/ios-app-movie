93612c76de00c8f4f7915e1b473e2446878ab495
Carlos Guillen <andres.ubri@mail.com>
2018-09-20 21:10:09 -0400

    Initial Commit

---
+++ Movie App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Movie App
+//
+//  Created by Carlos Andres on 9/20/18.
+//  Copyright © 2018 Carlos Andres. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
