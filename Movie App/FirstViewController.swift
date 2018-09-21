9408ce09ee5dfb00e321db88ef7506097143f390
Carlos Guillen <andres.ubri@mail.com>
2018-09-20 23:50:44 -0400

    Initial commit

--- Movie App/FirstViewController.swift
+++ Movie App/FirstViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    let movies = ["Transporter 1", "Transporter 2", "Transporter 3", "Transporter 4", "Transporter 5", "Transporter 5", "Transporter 5", "Transporter 5", "Transporter 5"];
+    
+        @IBOutlet weak var topRatedCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        topRatedCollectionView.dataSource = self;
+        topRatedCollectionView.delegate = self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +27,17 @@ class FirstViewController: UIViewController {
     }
 
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridItem", for: indexPath) as! GridItem;
+        
+        cell.titleLabel.text = movies[indexPath.item];
+        
+        return cell;
+    }
 }
 
---
+++ Movie App/Resources/GridItem/GridItem.swift
@@ -0,0 +1,16 @@
+//
+//  GridItem.swift
+//  Movie App
+//
+//  Created by Carlos Andres on 9/20/18.
+//  Copyright © 2018 Carlos Andres. All rights reserved.
+//
+
+import UIKit
+
+class GridItem: UICollectionViewCell {
+    
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+}
